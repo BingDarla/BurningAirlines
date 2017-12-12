1d7025a0820f24925aa2e69d2796afc0e9b476a8
BingDarla <hbshuang@gmail.com>
2017-12-12 16:06:53 +1100

    table done

--- db/migrate/20171212040301_create_airplanes.rb
+++ db/migrate/20171212040301_create_airplanes.rb
@@ -2,6 +2,8 @@ class CreateAirplanes < ActiveRecord::Migration[5.1]
   def change
     create_table :airplanes do |t|
       t.text :name
+      t.text :row
+      t.text :column
       t.timestamps
     end
   end
--- db/migrate/20171212040320_create_flights.rb
+++ db/migrate/20171212040320_create_flights.rb
@@ -1,7 +1,11 @@
 class CreateFlights < ActiveRecord::Migration[5.1]
   def change
     create_table :flights do |t|
-
+      t.text :flight_number
+      t.text :origin
+      t.text :destination
+      t.date :date
+      t.integer :airplane_id
       t.timestamps
     end
   end
